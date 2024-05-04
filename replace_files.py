d948c8b82ff8aefb8a84d634e570ebc9ff3a222d
alf <sh98allfass@gmail.com>
2024-05-04 22:49:37 +0500

    fix 4s numbers, remove _

--- replace_files.py
+++ replace_files.py
@@ -51,10 +51,15 @@ parent_directory = Path(args.source)
 current_files = [x for x in parent_directory.glob('**/*') if x.is_file()]
 new_path = Path(args.destination)
 new_path.mkdir(parents=False, exist_ok=True)
+for names in DATA:
+    if len(str(names["start"])) < 4:
+        names["start"] = f'0{names["start"]}'
+    if len(str(names["end"])) < 4:
+        names["end"] = f'0{names["end"]}'
 for file in current_files:
     for names in DATA:
-        if str(names["start"]) in file.name or str(names["end"]) in file.name:
+        if (str(names["start"])) in file.name or str(names["end"]) in file.name:
             shutil.copy(file, new_path)
             new_file = new_path / file.name
-            print(f'{names["name"].replace(" ", "_")}_{new_file.name}')
-            new_file.rename(Path(new_file.parent, f'{names["name"].replace(" ", "_")}_{new_file.name}'))
+            print(f'{names["name"]} {new_file.name}')
+            new_file.rename(Path(new_file.parent, f'{names["name"]} {new_file.name}'))
