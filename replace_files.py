8def806009e1c042ae8d75897985d3aa96e3b6e8
alf <sh98allfass@gmail.com>
2024-05-05 15:47:58 +0500

    fix comment

--- replace_files.py
+++ replace_files.py
@@ -3,7 +3,7 @@ import argparse
 import shutil
 
 DATA = [
-    { "name": "", "start": "", "end": "" },
+    { "name": "", "start": "", "end": "" }
 ]
 
 #parse arguments
@@ -19,11 +19,13 @@ parent_directory = Path(args.source)
 current_files = [x for x in parent_directory.glob('**/*') if x.is_file()]
 new_path = Path(args.destination)
 new_path.mkdir(parents=False, exist_ok=True)
+
 for names in DATA:
     if len(str(names["start"])) < 4:
         names["start"] = f'0{names["start"]}'
     if len(str(names["end"])) < 4:
         names["end"] = f'0{names["end"]}'
+
 for file in current_files:
     for names in DATA:
         if (str(names["start"])) in file.name or str(names["end"]) in file.name:
@@ -31,3 +33,4 @@ for file in current_files:
             new_file = new_path / file.name
             print(f'{names["name"]} {new_file.name}')
             new_file.rename(Path(new_file.parent, f'{names["name"]} {new_file.name}'))
+            
